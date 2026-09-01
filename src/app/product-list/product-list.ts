8c766df4bf4ad9102dd4fbef2390130df5203e1e
Akshay Jha <akshay.jha@kiya.ai>
2026-09-01 17:38:01 +0530

    chnage detect

--- src/app/product-list/product-list.ts
+++ src/app/product-list/product-list.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductService }
 from '../services/product';
@@ -14,7 +14,8 @@ export class ProductList {
   products:any[] = [];
 
   constructor(
-    private service: ProductService
+    private service: ProductService,
+    private cdr: ChangeDetectorRef
   ){}
 
   ngOnInit(){
@@ -23,6 +24,7 @@ export class ProductList {
       .subscribe(data => {
 
         this.products = data as any[];
+        this.cdr.detectChanges();
         console.log(this.products);
 
       });
