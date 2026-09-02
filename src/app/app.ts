d2deba0d0418010b2ee2495d2234411a8f444a6b
Akshay Jha <akshay.jha@kiya.ai>
2026-09-02 12:01:55 +0530

    added css

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+ 
 import {ProductList} from './product-list/product-list';
 
 @Component({
-  imports: [RouterOutlet,ProductList],
+  imports: [ProductList],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
